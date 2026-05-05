51060ef6fb34ba7b159370a1469b35d209b9377a
Aciad <dr.jekyll2004@gmail.com>
2026-05-05 17:54:20 -0600

    Prep 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,54 @@ using System;
 
 class Program
 {
+    static void GuessNumber(int magicNumber, ref bool done, ref int guesses)
+    {
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+        guesses ++;
+        if(magicNumber == guess){
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"That took you {guesses} guesses!");
+            done = false;}
+        else if (magicNumber < guess){
+            Console.WriteLine("Lower");
+        }
+        else if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+    }
+
+    static void Game()
+    {
+        int guesses = 0;
+        Console.Write("What is your magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        bool done = true;
+        while (done)
+        {
+            GuessNumber(magicNumber, ref done, ref guesses);
+        }
+
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool go = false;
+        do
+        {
+            Game();
+            Console.WriteLine("Would you like to go again? (y/n) ");
+            string goAgain = Console.ReadLine(); 
+            
+            if (goAgain == "yes" || goAgain == "Yes" || goAgain == "y" || goAgain == "Y")
+            {
+                go = true;
+            }
+            else
+            {
+                go = false;
+            }
+        }   while (go);
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Collections.ObjectModel;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> List = new();
+        ListMaker(List);
+        Console.WriteLine($"The sum is: {ListSum(List)}");
+        Console.WriteLine($"The average is: {ListAverage(List)}");
+        Console.WriteLine($"The largest number is: {ListGreatestInt(List)}");
+    }
+
+    static void ListMaker(List<int> list)
+    {
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (true) {
+            Console.Write("Enter a number: ");
+            list.Add(int.Parse(Console.ReadLine()));
+            if (list[^1] == 0)
+            {
+                break;
+            }
+        }
+    }
+
+    static int ListSum(List<int> list)
+    {
+        int sum = 0;
+        foreach (int number in list)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    static double ListAverage(List<int> list)
+    {
+        int sum = ListSum(list);
+        double average = sum / list.Count();
+        return average;
+    }
+
+    static int ListGreatestInt(List<int> list)
+    {
+        int greatest = 0;
+        foreach (int number in list)
+        {
+            if (greatest < number)
+            {
+                greatest = number;
+            }
+        }
+        return greatest;
     }
 }
